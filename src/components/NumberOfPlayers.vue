24744e1fc0dee3dc5980b78653c178b89a714409
Kevin Santana Medina <Kevin.Santana.Medina@es.ey.com>
2022-09-27 16:58:20 +0200

    matches for 1vs1 on the console, but very unfinished

--- src/components/NumberOfPlayers.vue
+++ src/components/NumberOfPlayers.vue
@@ -4,7 +4,6 @@
     <option>1vs1</option>
     <option>2vs2</option>
 </select>
-
     <div class="buttonInput">
         <input type="number" class="numer" v-model="numberOfPlayers" >
         <button @click="addPlayers" v-if="$store.state.numberOfPlayers > 0">Set players inputs</button>
