b778249fa8ae90ef7af937964c8c89459296494f
Kevin Santana Medina <Kevin.Santana.Medina@es.ey.com>
2022-09-28 09:17:13 +0200

    displaying names for matches 1vs1

--- src/components/MatchesList.vue
+++ src/components/MatchesList.vue
@@ -1,7 +1,7 @@
 <template>
-  <li v-for="(match, index) in $store.state.matches" v-bind:key="index" class="PlayerNameList">
-    <div class="matchCard"> <p>{{match.teams[0].members[0].name}} - {{match.teams[0].members[1].name}} <span> VS</span>  {{match.teams[1].members[0].name}} - {{match.teams[1].members[1].name}}</p> <div class="inputScore"><input type="text" v-on:input="setMatchScore($event, match.id)"> <input type="text" v-on:input="setMatchScore2($event, match.id)"> </div></div>
-      
+  <li v-for="(match, index) in $store.state.matches" v-bind:key="index" class="PlayerNameList" >
+    <div class="matchCard" v-if="$store.state.selectedMatchVS === '2vs2'"> <p>{{match.teams[0].members[0].name}} - {{match.teams[0].members[1].name}} <span> VS</span>  {{match.teams[1].members[0].name}} - {{match.teams[1].members[1].name}}</p> <div class="inputScore"><input type="text" v-on:input="setMatchScore($event, match.id)"> <input type="text" v-on:input="setMatchScore2($event, match.id)"> </div></div>
+    <div class="matchCard" v-if="$store.state.selectedMatchVS === '1vs1'"> <p>{{match[0].name }} <span> VS</span>  {{match[1].name}} </p> <div class="inputScore"><input type="text" v-on:input="setMatchScore($event, match.id)"> <input type="text" v-on:input="setMatchScore2($event, match.id)"> </div></div>
   </li>
   <div v-if="$store.state.selectedMatchVS === '1vs1'">
     <button @click="makeMatches1vs1" class="btn btn-primary" v-if="$store.state.currentPage > 0">display matches</button>
