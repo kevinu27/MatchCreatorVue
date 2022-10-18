df0dca32a21ada9abf006bfd372551524cd57d09
Kevin Santana Medina <Kevin.Santana.Medina@es.ey.com>
2022-10-18 10:56:44 +0200

    dropdown navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,14 +5,22 @@
                 <h1 class="navbar__logo"><router-link to="/">Match Creator</router-link></h1>
             <ul class="navbar__links">
                 <!-- <li><a href="#">Home</a></li> -->
-                <li><router-link to="/profile"> Profile</router-link></li>
+                <!-- <li><router-link to="/profile"> Profile</router-link></li> -->
+                <div class="dropdown">
+                  <li><router-link to="/profile"> User</router-link></li>
+                  <div class="dropdown-content">
+                    <a href="#">Profile</a>
+                    <a href="#">login</a>
+                    <a href="#">register</a>
+                    <a href="#">logout</a>
+                  </div>
+                </div>
                 <!-- <li><a href="https://codepen.io/cryptoctopus/collections/popular/">Pen Collection</a></li> -->
                 
             </ul>
             </div>
-            
         </header> 
-
+        
     </div>
 </template>
 
@@ -127,5 +135,46 @@ main {
 a.router-link-active {
     font-weight: 1000;
  
+}
+
+/* ---------------------- */
+.dropbtn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.dropdown:hover .dropbtn {
+  background-color: #3e8e41;
 }
 </style>
