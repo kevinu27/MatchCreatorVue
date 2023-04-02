95df48d9bce65402e11d89d2caf5d12777b10851
Kevin Santana Medina <Kevin.Santana.Medina@es.ey.com>
2023-04-02 16:08:14 +0200

    aparenteme funciona lo de las victorias para los inputs de la izqueirda, falta los de al derecha y que se muestren por pantalla

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -214,133 +214,44 @@ export default {
     const stateMatchesCopy = [...state.matches]
     const matchScoringPoints = stateMatchesCopy.find((match) => match.id === payload.id)
     matchScoringPoints.teams[0].points = points
+    if(matchScoringPoints.teams[0].points > matchScoringPoints.teams[1].points) {
+      matchScoringPoints.teams[0].victory = 1 
+      matchScoringPoints.teams[1].victory = 0
+    }
+    if(matchScoringPoints.teams[0].points < matchScoringPoints.teams[1].points) {
+      matchScoringPoints.teams[1].victory = 1 
+      matchScoringPoints.teams[0].victory = 0
+    }
     
     const matchesFlateados = state.matches.map((match) => match.teams).flat()
     // console.log('----matchesFlateados------!!!!', matchesFlateados)
-    // console.log('----state.matches------!!!!',  state.matches)
+    console.log('----state.matches------!!!!',  state.matches)
     for (let i = 0; i < state.newPlayers.length; i++) {
       let pointsArrayToPushToThePlayer = [0, 0]
+      let victoriesArrayToPushToThePlayer = [0, 0]
       const playerToPushThePointsId = state.newPlayers[i].playerIndex
       const reducer = (previousValue, currentValue) => previousValue + currentValue;
       for (let j = 0; j < matchesFlateados.length; j++) {
         if (playerToPushThePointsId === matchesFlateados[j].members[0].playerIndex) {
           pointsArrayToPushToThePlayer.push(matchesFlateados[j].points)
-          
+          victoriesArrayToPushToThePlayer.push(matchesFlateados[j].victory)
         }
         if (playerToPushThePointsId === matchesFlateados[j].members[1].playerIndex) {
           pointsArrayToPushToThePlayer.push(matchesFlateados[j].points)
+          victoriesArrayToPushToThePlayer.push(matchesFlateados[j].victory)
           
         }
       }
       state.newPlayers[i].points = pointsArrayToPushToThePlayer.reduce(reducer);
+      state.newPlayers[i].victory = victoriesArrayToPushToThePlayer.reduce(reducer);
     }
     const sortedPlayers = [...state.newPlayers].sort((a, b) => {
       return b.points - a.points;
     });
     state.newPlayers = sortedPlayers
-   
+   console.log('state.newPlayers---!-!_!-!_!-!-11-!-1-!1_', state.newPlayers)
   },
 
-  setMatchVictory_2vs2(state, payload ) {
-    const victory = 1 
-    const stateMatchesCopy = [...state.matches]
-    const matchScoringPoints = stateMatchesCopy.find((match) => match.id === payload.id)
-    if(matchScoringPoints.teams[0].points > matchScoringPoints.teams[1].points) {
-      matchScoringPoints.teams[0].members.victory = 1 
-      matchScoringPoints.teams[1].members.victory = 0
-    }
-    if(matchScoringPoints.teams[0].points < matchScoringPoints.teams[1].points) {
-      matchScoringPoints.teams[1].members.victory = 1 
-      matchScoringPoints.teams[0].members.victory = 0
-    }
-    
-    const matchesFlateados = state.matches.map((match) => match.teams).flat()
-    // console.log('----matchesFlateados------!!!!......', matchesFlateados)
-    console.log('----matchesstates------!!!!......!!!!!', state.matches)
-
-    // console.log('----state.matches------!!!!',  state.matches)
-    for (let i = 0; i < state.newPlayers.length; i++) {
-      let pointsArrayToPushToThePlayer = [0, 0]
-      const playerToPushThePointsId = state.newPlayers[i].playerIndex
-      const reducer = (previousValue, currentValue) => previousValue + currentValue;
-      for (let j = 0; j < matchesFlateados.length; j++) {
-        if (playerToPushThePointsId === matchesFlateados[j].members[0].playerIndex && matchesFlateados[j].victory) {
-          pointsArrayToPushToThePlayer.push(victory)
-          
-        }
-        if (playerToPushThePointsId === matchesFlateados[j].members[1].playerIndex && matchesFlateados[j].victory) {
-          pointsArrayToPushToThePlayer.push(victory)
-          
-        }
-      }
-      if(matchScoringPoints.teams[0].points > matchScoringPoints.teams[1].points) {
-        matchScoringPoints.teams[0].victory = 1 
-        matchScoringPoints.teams[1].victory = 0
-      }
-      if(matchScoringPoints.teams[0].points < matchScoringPoints.teams[1].points) {
-        matchScoringPoints.teams[1].victory = 1 
-        matchScoringPoints.teams[0].victory = 0
-      }
-      state.newPlayers[i].victory = pointsArrayToPushToThePlayer.reduce(reducer);
-    }
-    const sortedPlayers = [...state.newPlayers].sort((a, b) => {
-      return b.points - a.points;
-    });
-    state.newPlayers = sortedPlayers
-    
-    
-
-  },
-  setMatchVictory2_2vs2() {
-    // const victory = 1
-    // const stateMatchesCopy = [...state.matches]
-    // const matchScoringPoints = stateMatchesCopy.find((match) => match.id === payload.id)
-    // if(matchScoringPoints.teams[0].points > matchScoringPoints.teams[1].points) {
-    //   matchScoringPoints.teams[0].victory = 1 
-    //   matchScoringPoints.teams[1].victory = 0
-    // }
-    // if(matchScoringPoints.teams[0].points < matchScoringPoints.teams[1].points) {
-    //   matchScoringPoints.teams[1].victory = 1 
-    //   matchScoringPoints.teams[0].victory = 0
-    // }
-    
-    // const matchesFlateados = state.matches.map((match) => match.teams).flat()
-    // // console.log('----matchesFlateados------!!!!......', matchesFlateados)
-    // console.log('----matchesstates------!!!!......', state.matches)
-
-    // // console.log('----state.matches------!!!!',  state.matches)
-    // for (let i = 0; i < state.newPlayers.length; i++) {
-    //   let pointsArrayToPushToThePlayer = [0, 0]
-    //   const playerToPushThePointsId = state.newPlayers[i].playerIndex
-    //   const reducer = (previousValue, currentValue) => previousValue + currentValue;
-    //   for (let j = 0; j < matchesFlateados.length; j++) {
-    //     if (playerToPushThePointsId === matchesFlateados[j].members[0].playerIndex) {
-    //       pointsArrayToPushToThePlayer.push(victory)
-          
-    //     }
-    //     if (playerToPushThePointsId === matchesFlateados[j].members[1].playerIndex) {
-    //       pointsArrayToPushToThePlayer.push(victory)
-          
-    //     }
-    //   }
-    //   if(matchScoringPoints.teams[0].points > matchScoringPoints.teams[1].points) {
-    //     matchScoringPoints.teams[0].victory = 1 
-    //     matchScoringPoints.teams[1].victory = 0
-    //   }
-    //   if(matchScoringPoints.teams[0].points < matchScoringPoints.teams[1].points) {
-    //     matchScoringPoints.teams[1].victory = 1 
-    //   matchScoringPoints.teams[0].victory = 0
-    //   }
-    //   state.newPlayers[i].victory = pointsArrayToPushToThePlayer.reduce(reducer);
-    // }
-    // const sortedPlayers = [...state.newPlayers].sort((a, b) => {
-    //   return b.points - a.points;
-    // });
-    // state.newPlayers = sortedPlayers
-    
-    
-
-  },
 
   setMatchScore2_2vs2(state, payload) {
     console.log(" setMatchScore2_2vs2(state, payload){")
